1a1b62f92c87aca7367a281cf3b5ffcdee92bf43
webmaster <raffa.demello@gmail.com>
2020-07-05 13:52:36 -0300

    rm build folders

---
+++ wp-content/themes/portfolio/portfolio-react/precache-manifest.2891f4332fe9051f1e3295bb0e54829a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f7b85c7411278447e295d96daf8c47",
+    "url": "/index.php"
+  },
+  {
+    "revision": "0780b1d10ea450eb80b6",
+    "url": "/static/css/main.chunk.css"
+  },
+  {
+    "revision": "2f90694976c70e04e2ae",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "5e6862fff89a38aa3bb97be4a45871b6",
+    "url": "/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c4115a0a6c4ff3c50fc",
+    "url": "/static/js/bundle.js"
+  },
+  {
+    "revision": "0780b1d10ea450eb80b6",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "a1a16f9cc30168bd4a2ca384f4a156b7",
+    "url": "/static/media/profile.a1a16f9c.jpg"
+  }
+]);
