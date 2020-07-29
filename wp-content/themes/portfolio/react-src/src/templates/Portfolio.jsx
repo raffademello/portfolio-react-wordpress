503d38d46b6231ef4e46e868641b851d276ee27e
webmaster <raffa.demello@gmail.com>
2020-07-29 20:42:52 -0300

    ajustes css portfolio

--- wp-content/themes/portfolio/react-src/src/templates/Portfolio.jsx
+++ wp-content/themes/portfolio/react-src/src/templates/Portfolio.jsx
@@ -41,7 +41,7 @@ const useStyles = makeStyles(theme => ({
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3)
-  }
+  },
 }));
 
 const Portfolio = () => {
