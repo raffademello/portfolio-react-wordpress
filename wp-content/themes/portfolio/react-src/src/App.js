db171efa7c65f795e910dbbdd78339cd72e5893e
webmaster <raffa.demello@gmail.com>
2020-07-31 15:25:42 -0300

    ajustes na listagem do portfolio, locomoção do component dialog para fora do loop

--- wp-content/themes/portfolio/react-src/src/App.js
+++ wp-content/themes/portfolio/react-src/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { HashRouter } from "react-router-dom";
 import { loadCSS } from "fg-loadcss";
 import Routes from "./Routes";
@@ -8,40 +8,38 @@ import Footer from "./components/Footer";
 import SideMenu from "./components/SideMenu";
 import GoogleFontLoader from 'react-google-font-loader';
 
-function App() {
-  React.useEffect(() => {
-    loadCSS(
-      "https://use.fontawesome.com/releases/v5.12.0/css/all.css",
-      document.querySelector("#font-awesome-css")
-    );
-  }, []);
-  return (
-    <>
-    <GoogleFontLoader
-      fonts={[
-        {
-          font: 'Quattrocento Sans',
-          weights: [400, 700],
-        },
-        {
-          font: 'Roboto Mono',
-          weights: [400, 700],
-        },
-      ]}
-      subsets={['cyrillic-ext', 'greek']}
-    />
-      <HashRouter>
-            <SideMenu pageWrapId={"page-wrap"}/>
-            <div class="page-wrap">
-            <Navbar />
-            <Container maxWidth="md">
-              <Routes />
-            </Container>
-          </div>
-        <Footer />
-      </HashRouter>
-    </>
-  );
+export default class App extends Component {
+  componentDidMount(){
+    document.title = "Rafael de Melo - Desenvolvedor Web"
+  }
+  render() {
+    return (
+      <>
+      <GoogleFontLoader
+        fonts={[
+          {
+            font: 'Quattrocento Sans',
+            weights: [400, 700],
+          },
+          {
+            font: 'Roboto Mono',
+            weights: [400, 700],
+          },
+        ]}
+        subsets={['cyrillic-ext', 'greek']}
+      />
+        <HashRouter>
+              <SideMenu pageWrapId={"page-wrap"}/>
+              <div class="page-wrap">
+              <Navbar />
+              <Container maxWidth="md">
+                <Routes />
+              </Container>
+            </div>
+          <Footer />
+        </HashRouter>
+      </>
+    )
+  }
 }
 
-export default App;
